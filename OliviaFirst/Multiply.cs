8b97ed4c57ebfff6821a00c05fcea93dd8605369
swaran <singhsw@tcd.ie>
2016-12-29 17:44:54 +0000

    Add project files.

---
+++ OliviaFirst/Multiply.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OliviaFirst
+{
+    class Multiply
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter first number");
+            int firstNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number ");
+            int secondNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your answer is");
+            Console.WriteLine(firstNumber*secondNumber);
+
+            // Keep the console window open in debug mode.
+             Console.ReadKey();
+        }
+    }
+}
---
+++ OliviaFirst/SayingHello.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Hello
+    {
+        static void Main()
+        {
+            //String aboutOlivia = "Olivia goes to St Fiachrans";
+
+            int oliviaAge = 9;
+            int preetAge = 3;
+            int coleenAge = 3;
+            Console.WriteLine("Current Ages");
+            Console.WriteLine(oliviaAge+preetAge+coleenAge);
+
+
+           
+            Console.WriteLine("Multiply by 5");
+            int oliviaNewAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Answer");
+            Console.WriteLine(oliviaNewAge * 5);
+
+           
+
+        }
+    }
+}
