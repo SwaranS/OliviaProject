db60e8372ea467222092218c0c8c3d56c00084c4
olivia singh <olivia singh>
2016-12-31 12:22:38 +0000

    Use int subtraction & added to console. Age Subtraction.

---
+++ OliviaFirst/AgeByYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OliviaFirst
+{
+    class AgeByYear
+    {
+        static void Main() {
+            Console.WriteLine("HELLO WORLD");
+            int currentYear = 2016;
+            Console.WriteLine(currentYear);
+            Console.WriteLine("what year were you born?");
+
+            int personAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("your age is :");
+            Console.Write
+               (currentYear-personAge);
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
